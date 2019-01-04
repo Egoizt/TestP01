ac1e78cd1f1cd64b5b1d997225eed3cbce61efb2
Alexey Likhachev <egoiztic@gmail.com>
2019-01-05 00:15:40 +0300

    Brakeless speed management - added natural slowdown when acceleration pedal released - added acceleration rate - minor variables and functions rename
    Signed-off-by: Alexey Likhachev <egoiztic@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include "SPI.h"
 
 #define PIN_CS 10 //Chip-select pin for hardware SPI
-#define PIN_ACCEL A0
-#define PIN_STEER A1
+#define PIN_ACCELERATION A0
+#define PIN_STEERING A1
 
 #define POTENTIOMETER_LEFT_ADDRESS 0b00010001
 #define POTENTIOMETER_RIGHT_ADDRESS 0b00010010
 //#define POTENTIOMETER_SLOT_ALL_ADDRESS 0b00010011
 
-int accelValue;
-int steerValue;
-byte speedValue;
+const byte MAX_BYTE = 255;
+
+int accelerationRate;
+int steeringValue;
+byte currentSetSpeed = 0;
+double currentTrueSetSpeed = 0.0f;
+double accelerationSmoothness = 0.05f;
+double naturalSlowdownSmoothness = 0.5f;
 
 ///
 /// This method writes data to two Dais-Chain connected MCP4xxxx via SPI.
@@ -20,40 +25,62 @@ byte speedValue;
 ///     3. We send data for first MCP4xxxx, previous data will be pushed to second MCP4xxxx
 ///     and first one will get it's own data
 ///     4. Setting HIGH on CS releases MCP4xxxx so both of them can act
-/// \param addressFront
-/// \param addressRear
-/// \param valFront
-/// \param valRear
+/// \param address_front
+/// \param address_rear
+/// \param value_front
+/// \param value_rear
 ///
-void MCP4xxxxDaisyChainWrite(byte addressFront, byte addressRear, byte valFront, byte valRear) {
+void MCP4xxxxDaisyChainWrite(byte address_front, byte address_rear, byte value_front, byte value_rear) {
   digitalWrite(PIN_CS, LOW);
-  SPI.transfer(addressRear); // NOLINT
-  SPI.transfer(valRear); // NOLINT
-  SPI.transfer(addressFront); // NOLINT
-  SPI.transfer(valFront); // NOLINT
+  SPI.transfer(address_rear); // NOLINT
+  SPI.transfer(value_rear); // NOLINT
+  SPI.transfer(address_front); // NOLINT
+  SPI.transfer(value_front); // NOLINT
   digitalWrite(PIN_CS, HIGH);
 }
 
-byte getSpeed(int accel) {
-  //1024 – maximal analog input value which is converted to...
-  //...256 – maximal potenciometer accepted value
-  double speed = ((accel)/1024.0)*256.0;
-  return (byte)speed;
+double GetTargetSpeedFromAccellerationRate(int acceleration_rate) {
+  return ((acceleration_rate)/1023.0f)*255.0f;
+}
+
+double GetNewCurrentSpeed(int acceleration_rate, double current_speed, double acceleration_smoothness,
+    double natural_slowdown_smoothness) {
+  double target = GetTargetSpeedFromAccellerationRate(acceleration_rate);
+  double new_current;
+  if (target < current_speed) {
+    new_current = current_speed - (current_speed - target) * natural_slowdown_smoothness;
+  }
+  else if (target > current_speed) {
+    new_current = current_speed + (target - current_speed) * acceleration_smoothness;
+  }
+  else
+    new_current = current_speed;
+  return new_current;
+}
+
+int InvertAnalogPinValue(int value) {
+  return 1023 - value;
+}
+
+byte InvertByte(byte value) {
+  return MAX_BYTE - value;
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(PIN_CS, OUTPUT);
   SPI.begin(); // NOLINT
-  pinMode(PIN_ACCEL, INPUT);
-  pinMode(PIN_STEER, INPUT);
+  pinMode(PIN_ACCELERATION, INPUT);
+  pinMode(PIN_STEERING, INPUT);
 }
 
 void loop() {
-  accelValue = analogRead(PIN_ACCEL);
-  steerValue = analogRead(PIN_STEER);
-  speedValue = getSpeed(accelValue);
-  MCP4xxxxDaisyChainWrite(POTENTIOMETER_LEFT_ADDRESS, POTENTIOMETER_LEFT_ADDRESS, speedValue, speedValue);
-  MCP4xxxxDaisyChainWrite(POTENTIOMETER_RIGHT_ADDRESS, POTENTIOMETER_RIGHT_ADDRESS, speedValue, speedValue);
+  accelerationRate = InvertAnalogPinValue(analogRead(PIN_ACCELERATION));
+  steeringValue = analogRead(PIN_STEERING);
+  currentTrueSetSpeed = GetNewCurrentSpeed(accelerationRate, currentTrueSetSpeed, accelerationSmoothness,
+      naturalSlowdownSmoothness);
+  currentSetSpeed = InvertByte((byte)currentTrueSetSpeed);
+  MCP4xxxxDaisyChainWrite(POTENTIOMETER_LEFT_ADDRESS, POTENTIOMETER_LEFT_ADDRESS, currentSetSpeed, currentSetSpeed);
+  MCP4xxxxDaisyChainWrite(POTENTIOMETER_RIGHT_ADDRESS, POTENTIOMETER_RIGHT_ADDRESS, currentSetSpeed, currentSetSpeed);
   delay(50);
 }
